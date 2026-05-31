eb963f3017efec7350416b94acee0e480a5f71be
zeikar <zeikar@naver.com>
2026-05-31 13:16:27 +0900

    chore(companion): add render + realtime-avatar deps for Live2D

--- examples/companion/next.config.ts
+++ examples/companion/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@charivo/core", "@charivo/realtime", "@charivo/server"],
+  transpilePackages: [
+    "@charivo/core",
+    "@charivo/realtime",
+    "@charivo/realtime-avatar",
+    "@charivo/render",
+    "@charivo/render-live2d",
+    "@charivo/server",
+  ],
 };
 
 export default nextConfig;
